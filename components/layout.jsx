4f821c51d0da3c048e8a9f7cc2bd6f78ad5f85ef
Mauricio Romero <mauricionromero@hotmail.com>
2022-02-02 15:02:45 -0300

    added animated background

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,9 +3,16 @@ import Footer from "./footer";
 import styles from "./layout.module.scss";
 
 const layout = ({ children }) => {
+  let stars = [];
+  for (let i = 0; i < 50; i++) {
+    stars.push(<div className={styles.star} key={i}></div>);
+  }
   return (
     <>
       <Navbar />
+      <div className={styles.starsContainer}>
+        { stars }
+      </div>
       <main className={styles.container}>{children}</main>
       <Footer />
     </>
